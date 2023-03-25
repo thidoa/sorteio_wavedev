3a9f7f5a47267003b174bbc21ce25f2e0c7c12a3
Thidoa <thiagovini200@gmail.com>
2023-03-25 18:24:31 -0300

    style: arquivos do front adicionados

--- sorteio/urls.py
+++ sorteio/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('sorteio', views.sorteio, name="sorteio"),
+    path('sobre', views.sobre, name="sobre"),
 ]
--- sorteio/views.py
+++ sorteio/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def sorteio(request):
+    return render(request, 'sorteio.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
